f19870636c67fb59005d23ae4dc8d41e90ba269a
Oluwadamiloju Yusuf <yusufdamiloju@gmail.com>
2022-04-03 20:51:26 +0300

    Final

--- mma8653fc_driver.c
+++ mma8653fc_driver.c
@@ -306,6 +306,22 @@ float convert_to_g(uint16_t raw_val, uint8_t sensor_scale)
 {
     float res;
     // TODO Convert raw sensor data to g-force acceleration value
+    if (sensor_scale == 2)
+    {
+        res = (1000 x raw_val + 1024) >> 6;
+    }
+    else if (sensor_scale == 4)
+    {
+        res = (1000 x raw_val + 512) >> 5;
+    }
+    else if (sensor_scale == 8)
+    {
+        res = (1000 x raw_val + 256) >> 4;
+    }
+    else
+    {
+        res = 0;
+    }
 
-    return res;
+    return res / 1000;
 }
